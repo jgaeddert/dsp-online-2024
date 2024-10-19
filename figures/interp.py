7a88ff3bee4e9f43c24ec5ff4b20bc435d931bf6
Joseph D. Gaeddert <joseph@liquidsdr.org>
2024-10-19 14:07:53 -0400

    interp: adding option to plot cross-correlation values

--- figures/interp.py
+++ figures/interp.py
@@ -11,6 +11,7 @@ p.add_argument('-plotsyms',action='store_true',   help='enable plotting symbols'
 p.add_argument('-nstd',    default=0, type=float, help='noise standard deviation')
 p.add_argument('-fc',      default=0, type=float, help='noise standard deviation')
 p.add_argument('-plotcos', action='store_true',   help='enable plotting cosine of carrier offset')
+p.add_argument('-plotcor', action='store_true',   help='enable plotting cross-correlation')
 args = p.parse_args()
 
 # initialization
@@ -24,7 +25,7 @@ interp = dsp.firinterp(M, m, As)
 
 # generate random symbols and interpolate
 symbols = rng.choice(modmap,num_symbols).astype(np.csingle)
-samples = interp.execute(np.concatenate((symbols,np.zeros(2*m))))
+signal  = interp.execute(np.concatenate((symbols,np.zeros(2*m))))
 
 num_samples = (num_symbols+2*m)*M
 
@@ -32,13 +33,16 @@ t0 = np.arange(num_symbols)
 t1 = np.arange(num_samples)/M - m
 
 phasor = np.exp(2j*np.pi*args.fc*t1)
-samples *= phasor
+samples = signal * phasor
 
 noise = rng.normal(0,args.nstd,2*num_samples).astype(np.single).view(np.csingle)
 samples += noise
 
 # plot time series
-fig, ax = plt.subplots(1,figsize=(12,4))
+if args.plotcor:
+    fig, (ax,ax2) = plt.subplots(2,1,figsize=(12,8))
+else:
+    fig, ax = plt.subplots(1,figsize=(12,4))
 ax.plot(t1, np.real(samples))
 if not args.plotsyms:
     ax.plot(t1, np.imag(samples))
@@ -52,6 +56,17 @@ if args.plotsyms:
 
 if args.plotcos:
     ax.plot(t1, np.real(phasor), ':', linewidth=0.5, color='black')
+
+if args.plotcor:
+    '''plot cross correlation vs. time lag'''
+    rxy = np.fft.fftshift( np.fft.ifft( np.fft.fft(samples) * np.conj(np.fft.fft(signal)) ))
+    #print('rxy',max(abs(rxy)),'n',len(samples), 'E{s^2}', np.sum(np.abs(signal)**2),)
+    rxy /= np.sum(np.abs(signal)**2)
+    txy = np.arange(num_samples)/M - m - num_symbols/2
+    ax2.plot(txy, np.real(rxy), txy, np.imag(rxy))
+    ax2.set_xlabel('Lag [symbols]')
+    ax2.set_ylabel('Cross Correlation')
+    ax2.set(xlim=(-num_symbols/2-m,num_symbols/2+m),ylim=(-1.1,1.1))
 
 if args.output is not None:
     fig.savefig(args.output, dpi=200, bbox_inches='tight')
--- makefile
+++ makefile
@@ -20,12 +20,12 @@ figures :=			\
 interp.png          : figures/interp.py    ; ./$< -o $@ -plotsyms
 interp_clean.png    : figures/interp.py    ; ./$< -o $@
 interp_noise.png    : figures/interp.py    ; ./$< -o $@ -nstd 0.1
-interp_rot_0.png    : figures/interp.py    ; ./$< -o $@ -plotcos -fc 0
-interp_rot_1.png    : figures/interp.py    ; ./$< -o $@ -plotcos -fc 0.0005
-interp_rot_2.png    : figures/interp.py    ; ./$< -o $@ -plotcos -fc 0.00208333
-interp_rot_3.png    : figures/interp.py    ; ./$< -o $@ -plotcos -fc 0.00416667
-interp_rot_4.png    : figures/interp.py    ; ./$< -o $@ -plotcos -fc 0.00833333
-interp_rot_5.png    : figures/interp.py    ; ./$< -o $@ -plotcos -fc 0.02
+interp_rot_0.png    : figures/interp.py    ; ./$< -o $@ -plotcos -plotcor -fc 0
+interp_rot_1.png    : figures/interp.py    ; ./$< -o $@ -plotcos -plotcor -fc 0.0005
+interp_rot_2.png    : figures/interp.py    ; ./$< -o $@ -plotcos -plotcor -fc 0.00208333
+interp_rot_3.png    : figures/interp.py    ; ./$< -o $@ -plotcos -plotcor -fc 0.00416667
+interp_rot_4.png    : figures/interp.py    ; ./$< -o $@ -plotcos -plotcor -fc 0.00833333
+interp_rot_5.png    : figures/interp.py    ; ./$< -o $@ -plotcos -plotcor -fc 0.02
 
 partition.png       : figures/partition.py ; ./$< -o $@
 partition_comp.png  : figures/partition.py ; ./$< -o $@ -plotcomp
