86614befdf83abfaca089c02dec5f249f2566c90
Joseph D. Gaeddert <joseph@liquidsdr.org>
2024-10-19 09:07:23 -0400

    adding basic interp script for testing

--- README.md
+++ README.md
@@ -4,4 +4,11 @@ Against The Clock
 
 This repository holds the source code for generating...
 
+Setup
+-----
 
+    virtualenv ~/env/against-the-clock -p python3.12
+    source ~/env/against-the-clock/bin/activate
+    pip install -r requirements.txt
+
+Note: supported versions of python up to 3.12
---
+++ figures/interp.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import liquid as dsp
+import numpy as np
+import matplotlib.pyplot as plt
+
+# initialization
+modmap = np.array((1+1j,1-1j,-1+1j,-1-1j))
+rng    = np.random.default_rng(12345)
+
+# design interpolator from prototype
+M, m, As, num_symbols = 4, 6, 60., 120
+interp = dsp.firinterp(M, m, As)
+
+# generate random symbols and interpolate
+symbols = rng.choice(modmap,num_symbols).astype(np.csingle)
+samples = interp.execute(np.concatenate((symbols,np.zeros(2*m))))
+
+# plot results
+t0 = np.arange(num_symbols)
+t1 = np.arange((num_symbols+2*m)*M)/M - m
+
+# plot impulse and spectral responses
+fig, (axi, axq) = plt.subplots(2,figsize=(8,8))
+for ax,func,label in ((axi,np.real,'Real'),(axq,np.imag,'Imag')):
+    ax.plot(t1, func(samples))
+    ax.plot(t0, func(symbols), 'o')
+    ax.set_xlabel('Time [samples]')
+    ax.set_ylabel(label)
+    ax.grid(True, zorder=5)
+
+plt.show()
+
+
